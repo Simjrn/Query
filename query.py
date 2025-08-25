ed30f8d6325281204b5f818c1e083bdc16c951d0
Simjrn <simonjenni374@gmail.com>
2025-08-25 19:11:32 +0100

    Update query.py

--- query.py
+++ query.py
@@ -12,3 +12,9 @@ def ask_chat(prompt):
 def ask_alert(prompt):
   answer = input(prompt)
 
+def three_choice(answer, prompt1, prompt2, prompt3, alignment):
+  flex = st.container(horizontal=True, horizontal_alignment=alignment)
+  for card in range(3):
+    flex.button(prompt1)
+    flex.button(prompt2)
+    flex.button(prompt3)
