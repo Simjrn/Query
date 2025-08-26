656479123ad5377275fa5773048ffbdc5a2f7c5b
Simjrn <simonjenni374@gmail.com>
2025-08-26 09:21:14 +0100

    Update query.py

--- query.py
+++ query.py
@@ -15,6 +15,27 @@ def ask_alert(prompt):
 def three_choice(answer, prompt1, prompt2, prompt3, alignment):
   flex = st.container(horizontal=True, horizontal_alignment=alignment)
   for card in range(3):
-    flex.button(prompt1)
-    flex.button(prompt2)
-    flex.button(prompt3)
+    if flex.button(prompt1):
+      answer = 1
+    elif flex.button(prompt2):
+      answer = 2
+    elif flex.button(prompt3):
+      answer = 3
+
+def dialog_ask(title, caption):
+    @st.dialog(title)
+    def ask_input(caption):
+        answer = st.text_input(caption)
+    ask(caption)
+
+def dialog_area(title, caption):
+    @st.dialog(title)
+    def ask_area(caption):
+        answer = st.text_area(caption)
+    ask(caption)
+
+def dialog_chat(title, caption):
+    @st.dialog(title)
+    def ask(caption):
+        answer = st.chat_input(caption)
+    ask(caption)
