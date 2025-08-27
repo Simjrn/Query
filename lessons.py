2c2aafd210983ec9483a196ffd8918bb09bfad29
Simjrn <simonjenni374@gmail.com>
2025-08-27 09:54:37 +0100

    Update lessons.py

--- lessons.py
+++ lessons.py
@@ -1,2 +1,8 @@
-import query
+import query as qy
 
+with st.sidebar:
+  st.header("Choose your lesson")
+  lesson = st.radio("Section 1:", ["lesson 1", "lesson 2", "lesson 3"])
+
+if lesson == "lesson 1":
+  
